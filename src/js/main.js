cc1f25b96f6c0b82edc8bacfad1fbfbbf55d6819
ndurgesh88 <durgeshnidadavolu@gmail.com>
2024-02-14 00:03:47 +0800

    Promote a Link card basic structure of JS created

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
-console.log('Testing APP!');
+// <<---- Promote a Link JS ----->>
+
+// Grab node elements 'Buttons'
+const createBtn = document.getElementById('btnCreate');
+
+// Grab Form, elements 'Input, Select'
+const getFormPromoteLink = document.getElementById('promoteLinkForm');
+const getInputUrl = document.getElementById('urlInput');
+const selectBrandTheme = document.getElementById('themeDropdown');
+selectBrandTheme.addEventListener('change', (event) => {
+  console.log(event.target.value == 0);
+});
+
+// Validate Form
+
+const validateForm = () => {
+  let isUrlValid = getInputUrl.value.trim().length > 0;
+  let isThemeSelected = selectBrandTheme.value !== '';
+  createBtn.disabled = !(isUrlValid && isThemeSelected);
+};
+
+getInputUrl.addEventListener('input', validateForm);
+getInputUrl.addEventListener('change', validateForm);
+
+validateForm();
